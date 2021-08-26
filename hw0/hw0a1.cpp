905d0fbaa57964a3fdcd2cfffe87be77311ccf05
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-08-26 15:28:47 -0800

    added additional program 1

---
+++ hw0/hw0a1.cpp
@@ -0,0 +1,16 @@
+// "Faith" is a fine invention. Emily Dickinson
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n\"Faith\" is a fine invention\n";
+    cout << "By Emily Dickinson\n\n";
+
+    cout << "Faith is a fine invention\n";
+    cout << "For Gentlemen who see!\n";
+    cout << "But Microscopes are prudent\n";
+    cout << "In an Emergency!\n";
+}
