b432a290ddbbf60073e4f8aea0bbc13133397005
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-08-26 15:48:56 -0800

    added additional program 2

---
+++ hw0/hw01a2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "One of my favorite games that I use to play is the card game: Rummy.\n";
+    cout << "Each player tries to form matched sets consiting of groups of three or ";
+    cout << "four of a kind,\nor sequences of three or more cards of the same suite.\n";
+    return 0;
+}
