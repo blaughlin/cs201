95838312489997f63af0d952799ea16d1d60e397
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-08-30 14:14:08 -0800

    ignore .vscode folder again

---
+++ lab01/lab01.cpp
@@ -0,0 +1,17 @@
+/* Lab 01.cpp
+Bernard Laughlin
+08/30/2021
+Simple Hello world program for cs201 */
+
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+    int a = 10;
+    int b = 100;
+    std::cout << "Hello, World!\n";
+    std:cout << a << " multiplied by " << b << " = " << a*b << "\n";
+    return 0;
+}
