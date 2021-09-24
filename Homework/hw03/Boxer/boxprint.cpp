b116309ea7ad0fa1d386d46002f7414f465f7a39
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-09-24 12:16:34 -0800

    added ability to continue creating boxes until quit is typed in

--- Homework/hw03/Boxer/boxprint.cpp
+++ Homework/hw03/Boxer/boxprint.cpp
@@ -15,9 +15,15 @@ using std::endl;
 int main() {
     int layers;
     string message;
-
-    cout << "Enter message followed by a space and number of layers to print: ";
-    cin >> message >> layers;
-    PrintBox(layers, message);
+    bool isDone = false;
+    while (!isDone) {
+        cout << "Enter message followed by a space and number of layers to print: ";
+        cin >> message >> layers;
+        if (message == "quit") {
+            isDone = true;
+            break;
+        }
+        PrintBox(layers, message);
+    }
     return 0;
 }
