75c762ee481f917d342ffbeb63b48ef439acdb86
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-09-26 13:41:22 -0800

    refactored code and add PrintResult function

--- Homework/hw03/Collatz/collatz.cpp
+++ Homework/hw03/Collatz/collatz.cpp
@@ -15,25 +15,14 @@ bool IsOdd(int n){
     return (n % 2 != 0);
 }
 
-// collatz.cpp
-// Bernard Laughlin 9/26/2021
-// implements the collatz function [L. Collatz 1937]
-
-#include <iostream>
-#include <string>
-
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
-
-// returns true if input is odd
-bool IsOdd(int n){
-    return (n % 2 != 0);
-}
-
 // Prints int without a comma if input is one, else prints with a comma
-
+void PrintResults(int input) {
+    if (input == 1) {
+        cout << input;
+    } else {
+        cout << input << ", ";
+    }
+}
 
 int main() {
     int result;
@@ -45,22 +34,13 @@ int main() {
         cout << "Invalid input. Input a positive integer: ";
         cin >> result;
     }
-    if (input == 1) {
-        cout << input;
-    } else {
-        cout << input << ", ";
-    }
+    PrintResults(result);
     while (result != 1) {
         if (IsOdd(result)){
             result = 3 * result + 1;
         } else {
             result = result/2;
         }
-        if (input == 1) {
-            cout << input;
-        } else {
-            cout << input << ", ";
-        }
+        PrintResults(result);
     }
     return 0;
-}
