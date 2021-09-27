b269f5dbf5274224ef01a6481a91ce932fc425c6
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-09-26 18:35:00 -0800

    added kelvin.cpp

---
+++ Homework/hw03/BrokenKelvin/cmake-build-debug/CMakeFiles/BrokenKelvin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BrokenKelvin"
+  "BrokenKelvin.pdb"
+  "CMakeFiles/BrokenKelvin.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BrokenKelvin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Homework/hw03/BrokenKelvin/kelvin.cpp
@@ -0,0 +1,21 @@
+// kelvin.cpp
+// Bernard Laughlin 9-26-2021
+// Converts celsius into kelvin
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::end;
+
+
+int main() {
+    double  = 0;
+    cin >> d;
+    return 0;
+}
+
+// Error in original code
+// 1. ctok declase an int k instead of a double
+// 2. ctok function returns an int instead of a double
+// 3. a double c is declared but never used
+// 4. cin trys to assign an input value to an undeclared variable d
+// 5. ctok passes a character as a parameter instead of an int.
