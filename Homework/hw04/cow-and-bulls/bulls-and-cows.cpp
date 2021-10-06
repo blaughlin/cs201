845dcdb115dd32eb54f62eaf4b9ff15c60b35d71
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-10-05 17:38:37 -0800

    added check to see it person won the game in bulls-and-cows.cpp

--- Homework/hw04/cow-and-bulls/bulls-and-cows.cpp
+++ Homework/hw04/cow-and-bulls/bulls-and-cows.cpp
@@ -36,25 +36,35 @@ vector<int> GenerateRandomInts(int amount) {
     return numbers;
 }
 
+// Prompts user for guess it appends negative 1 to return vector if a negative number
+// is found or negative 2 if invalid input.
 vector<int> AskForGuess() {
     vector<int> guess;
     std::string input;
     int x;
-    cout << "Please enter 4 numbers 0 to 9 or a negative number to quit: ";
+    cout << "Please enter 4 unique numbers from 0 to 9 or a negative number to quit: ";
     cin >> input;
     istringstream instream(input);
     instream >> x;
     if (!instream) {
         cout << "Invalid input, text needs to be a number" << endl;
+        guess.push_back(-2);
+        return guess;
     } else {
         for (auto i : input) {
             // if negative number found return negative -1
-            if (i - '0' <= 0) {
+            if (i - '0' < 0) {
                 guess.push_back(-1);
                 return guess;
+                // if there is text followed entered after an integer
             } else if (i <48 || i > 57 ) {
                 guess.push_back(-2);
                 cout << "Please only enter integers" << endl;
+                return guess;
+                // if user entered same number again
+            } else if (std::find(guess.begin(), guess.end(), i - '0') != guess.end()) {
+                guess.push_back(-2);
+                cout << "Please only enter unique integers" << endl;
                 return guess;
             }
             guess.push_back(i - '0');
@@ -63,26 +73,71 @@ vector<int> AskForGuess() {
     return guess;
 }
 
+// Returns the number of correctly match guesses
+int CheckForBulls(vector<int> key, vector<int> guess) {
+    int bulls = 0;
+    for (auto i = 0; i < key.size(); i++){
+        if (key[i] == guess[i]) {
+            bulls += 1;
+        }
+    }
+    return bulls;
+}
+
+int CheckForCows(vector<int> key, vector<int> guess) {
+    int cows = 0;
+    int bulls = CheckForBulls(key, guess);
+    for (auto i : guess) {
+        if ( std::find(key.begin(), key.end(), i) != key.end()) {
+            cows += 1;
+        }
+    }
+    return cows - bulls;
+}
+
 int main() {
+    bool isInvalid = false;
     bool gameOver = false;
+    int bulls = 0;
+    int cows = 0;
+    std::string bullStr = " bull ";
+    std::string cowStr = " cow";
     vector<int> key = GenerateRandomInts(4);
     vector<int> guess;
     for (auto i : key) {
         cout << i << endl;
     }
     while (!gameOver) {
+        isInvalid = false;
         guess = AskForGuess();
         for (auto i : guess) {
-            cout << i << endl;
             // end game if negative number found
             if (i == -1) {
+                cout << i;
                 gameOver = true;
-            // break out of for loop if person entered numbers followed by letters
-            } else if ( i == -2 ){
+                // break out of for loop if input invalid
+            } else if (i == -2) {
+                isInvalid = true;
                 break;
             }
         }
+        // Print out guess
+        if (!isInvalid && !gameOver) {
+            cout << "You guessed: ";
+            for (auto i : guess){
+                cout << i;
+            }
+            cout << endl;
+            bulls = CheckForBulls(key, guess);
+            cows = CheckForCows(key, guess);
+            if (bulls > 1 ) bullStr = " bulls ";
+            if (cows > 1 ) cowStr = " cows";
+            cout << bulls  << bullStr << cows << cowStr << endl;
+            if (bulls == 4) {
+                cout << "You Won!" << endl;
+                gameOver = true;
+            }
+        }
     }
-
     return 0;
 }
