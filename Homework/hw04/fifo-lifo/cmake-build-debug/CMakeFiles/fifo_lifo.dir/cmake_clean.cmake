9ac4dd3b8543874983e26ccf380536ada16cba7e
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-10-07 11:37:32 -0800

    first commit of fifo_lifo

---
+++ Homework/hw04/fifo-lifo/cmake-build-debug/CMakeFiles/fifo_lifo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fifo_lifo.dir/main.cpp.o"
+  "fifo_lifo"
+  "fifo_lifo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fifo_lifo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Homework/hw04/fifo-lifo/fifo-lifo.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+void FifoPush(vector<string> & container, const string & item){
+
+}
+
+void FifoPop(vector<string> & container,  string & item){
+
+}
+
+void LifoPush(vector<string> & container, const string & item){
+
+}
+
+void LifoPop(vector<string> & container,  string & item){
+
+}
+
+bool IsContainerEmpty(const vector<string> & container){
+
+}
+
+void PrintContainer(const vector<string> & container){
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
