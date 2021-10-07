304dd147ad065dd7e898ce4b42bd7aa418b157dc
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-10-07 13:30:31 -0800

    added TestFifo()

--- Homework/hw04/fifo-lifo/fifo-lifo.cpp
+++ Homework/hw04/fifo-lifo/fifo-lifo.cpp
@@ -17,8 +17,8 @@ bool IsContainerEmpty(const vector<string> & container){
 
 // Appends item to beginning of vector
 void FifoPush(vector<string> & container, const string & item){
-    auto it = container.begin();
-    it = container.insert(it, item);
+    container.push_back(item);
+
 }
 
 // Removes first element from vector and assigns it to item
@@ -33,20 +33,19 @@ void FifoPop(vector<string> & container,  string & item){
 
 // appends item to end of container
 void LifoPush(vector<string> & container, const string & item){
-    container.push_back(item);
+    auto it = container.begin();
+    it = container.insert(it, item);
 }
 
 // Removes last element from vector and assigns it to item
 void LifoPop(vector<string> & container,  string & item) {
     if (!IsContainerEmpty(container)) {
-        item = container[container.size() - 1];
-        container.pop_back();
+        item = container[0];
+        container.erase(container.begin());
     } else {
         cout << "Error: Container is empty!" << endl;
     }
 }
-
-
 
 // Prints out content of container
 void PrintContainer(const vector<string> & container){
@@ -56,14 +55,57 @@ void PrintContainer(const vector<string> & container){
     cout << endl;
 }
 
+bool TestFifo() {
+    bool pass = false;
+    string item;
+    vector<string> key = {"A", "B", "C", "D"};
+    vector<string> test;
+    FifoPush(test, "A");
+    FifoPush(test, "B");
+    FifoPush(test, "C");
+    FifoPush(test, "D");
+    if (test == key) pass = true;
+    FifoPop(test, item);
+    if (item == "A"){
+        pass = true;
+    } else {
+        pass = false;
+    }
+    FifoPop(test, item);
+    if (item == "B"){
+        pass = true;
+    } else {
+        pass = false;
+    }
+    FifoPop(test, item);
+    if (item == "C"){
+        pass = true;
+    } else {
+        pass = false;
+    }
+    FifoPop(test, item);
+    if (item == "D"){
+        pass = true;
+    } else {
+        pass = false;
+    }
+    return pass;
+}
+
 int main() {
     string myString;
     vector<string> empty;
     vector<string> myVector = {"one", "two", "three"};
-    LifoPush(myVector, "four");
-    PrintContainer(myVector);
-    LifoPop(myVector, myString);
-    PrintContainer(myVector);
-    cout << myString;
+    LifoPush(empty, "A");
+    LifoPush(empty, "B");
+    LifoPush(empty, "C");
+    LifoPush(empty, "D");
+    PrintContainer(empty);
+    LifoPop(empty, myString);
+    PrintContainer(empty);
+
+
+
+    cout << TestFifo();
     return 0;
 }
