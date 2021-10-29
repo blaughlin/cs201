0f2362cda580cb3846e1c08d48f46e8d85f00892
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-10-29 13:33:36 -0800

    Added GetStats() to vector-stats.cpp

---
+++ Homework/hw5/vector-stats/cmake-build-debug/CMakeFiles/vector_stats.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector_stats.dir/main.cpp.o"
+  "vector_stats"
+  "vector_stats.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector_stats.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Homework/hw5/vector-stats/main.cpp
@@ -0,0 +1,52 @@
+// vector-stats
+// Bernard Laughlin 10/29/2021
+// HW5 for cs201
+// Write a function that finds the smallest and largest elements of a vector argument and also
+// computes the mean and median. Return the min, max, mean and median using a struct. Do
+// not use global variables. For testing purposes, write a main program that calls this function with
+// at least 5 different vectors
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
+
+struct Stats {
+    double min;
+    double max;
+    double median;
+    double mean;
+};
+
+// Returns min, max, median, and mean of a vector
+Stats GetStats(const vector<double> & input){
+    Stats results;
+    // get min
+    results.min = input.at(0);
+    for (const auto & i : input){
+        if (i < results.min) results.min = i;
+    }
+
+    // get max
+    results.max = input.at(0);
+    for (const auto & i : input){
+        if (i > results.max) results.max = i;
+    }
+
+    // get mean
+    double sum = 0;
+    for (const auto & i : input){
+        sum += i;
+    }
+    results.mean = sum / input.size();
+
+    // get median
+    results.median = input.at(input.size()/2);
+
+    return results;
+}
+
+int main() {
+    vector<double> a {1,2,3,4,5};
+    return 0;
+}
