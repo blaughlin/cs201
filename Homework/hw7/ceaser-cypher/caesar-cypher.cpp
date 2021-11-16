3978a83daf0ecb6962bfc12507ae0dd564d8495e
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-11-16 13:09:24 -0900

    added GetInput() to caesar-cypher.cpp

---
+++ Homework/hw7/ceaser-cypher/caesar-cypher.cpp
@@ -0,0 +1,78 @@
+// caesar-cypher.cpp
+// Bernard Laughlin 11/16/2021
+// A C++ program that repeatedly inputs a line of text and an integer used a shift. It
+// applies the Caesar Cypher with that shift to the text and output the result.
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+#include<sstream>
+using std::istringstream;
+using std::getline;
+
+// gets user input
+bool GetInput(string & msg) {
+    string input1;
+    while (true) {
+        cout << "Enter message to encode or 'q' to quit: ";
+        getline(cin, input1);
+        istringstream istream(input1);
+        istream >> msg;
+        if (msg == "q") {
+            return true;
+        }
+        if (!istream) cout << "Invalid input." << endl;
+        else break;
+    }
+    return false;
+}
+
+//    cout << "Enter message to encode or 'q' to quit: ";
+//    getline(cin, input1);
+//    istringstream istream(input1);
+//    istream >> msg;
+//    if(!istream) cout << "Invalid input." << endl;
+//    if (msg == "q") {
+//        isDone = true;
+//        return false;
+//    }
+//    cout << "Enter shift number ";
+//    getline(cin, input2);
+//    istringstream istream2(input2);
+//    istream2 >> shift;
+//    if(!istream2) cout << "Invalid input." << endl;
+//    return true;
+//}
+
+// Shifts every letter in message by shift count.
+void Encode(const string & msg, const int & shift) {
+    for (auto i : msg) {
+        // handle upper case
+        if (i >= 65 && i <= 90) {
+            if ( i + shift > 90){
+                cout << char(65+(i + shift - 65) % 26);
+            } else cout << char(i + shift);
+            // handle lower case
+        } else if (i >= 97 && i <= 122) {
+            if ( i + shift > 122){
+                cout << char(97+(i + shift - 97) % 26);
+            } else cout << char(i + shift);
+            // everything else
+        } else cout << i;
+    }
+    cout << endl;
+}
+
+
+int main() {
+    string message;
+    int shift = 0;
+    while (!GetInput(message)){
+        Encode(message, shift);
+    }
+
+
+    return 0;
+}
---
+++ Homework/hw7/ceaser-cypher/cmake-build-debug/CMakeFiles/ceaser_cypher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ceaser_cypher.dir/caesar-cypher.cpp.o"
+  "ceaser_cypher"
+  "ceaser_cypher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ceaser_cypher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
