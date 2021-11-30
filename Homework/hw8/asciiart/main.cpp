cda0cd73b7f06707fbacc38b82cc7f0f76b9ee6f
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-11-30 11:25:21 -0900

    Added ReadFile() to RGBImage class in asciiart.cpp

--- Homework/hw8/asciiart/main.cpp
+++ Homework/hw8/asciiart/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using std::cout;
+using std::endl;
+#include <fstream>
+using std::ifstream;
 #include <string>
 using std::string;
+using std::getline;
+#include <vector>
+using std::vector;
+#include <sstream>
+using std::stringstream;
+#include <map>
+using std::map;
+
 class Pixel {
 public:
     Pixel(int red, int green, int blue) :
@@ -15,8 +27,59 @@ class RGBImage {
 public:
     RGBImage(string filename) :
     _file{filename} {}
+
+    void ReadFile() {
+        int red, green, blue;
+        int counter = 0;
+        int colorCount = 0;
+        ifstream fin(_file);
+        if (!fin) {
+            cout << "Can not open file" << endl;
+        } else {
+            while (true) {
+                string line;
+                getline(fin, line);
+                counter++;
+                if (counter == 3) {
+                    stringstream(line) >> _width;
+                    stringstream(line) >> _height;
+                }
+                if (counter >= 4) {
+                    if (colorCount == 0) {
+                        stringstream(line) >> red;
+                        colorCount += 1;
+                    } else if (colorCount == 1) {
+                        stringstream(line) >> green;
+                        colorCount += 1;
+                    } else if (colorCount == 2) {
+                        stringstream(line) >> blue;
+                        colorCount = 0;
+                        Pixel color(red, green, blue);
+                        _image.push_back(color);
+                    }
+                }
+                if (!fin) {
+                    if (fin.eof()) {
+                        cout << "Finished reading file." << endl;
+                    } else {
+                        cout << "Error during transmit" << endl;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    void toASCII(){
+        ReadFile();
+
+    }
+
 private:
     string _file;
+    int _width;
+    int _height;
+    vector<Pixel> _image;
 };
 
 int main() {
